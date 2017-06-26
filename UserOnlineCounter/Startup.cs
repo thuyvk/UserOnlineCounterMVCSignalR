9beae0108b328f6ada018a4dadc61177acd27916
Thuyvk's Blog <khanhthuy46k2dhv@gmail.com>
2017-06-26 11:55:16 +0700

    second commit

---
+++ UserOnlineCounter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UserOnlineCounter.Startup))]
+namespace UserOnlineCounter
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
