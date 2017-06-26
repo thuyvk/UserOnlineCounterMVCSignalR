30a918aed6459e0524b9335f75176abb22b9fb76
Thuyvk's Blog <khanhthuy46k2dhv@gmail.com>
2017-06-26 11:43:23 +0700

    first commit

---
+++ UserOnlineCounter/Hubs/CounterHub.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace thuyvk.com.Hubs
+{
+    public class CounterHub : Hub
+    {
+        private static int counter = 0;
+
+        public override System.Threading.Tasks.Task OnConnected()
+        {
+            counter = counter + 1;
+            Clients.All.UpdateCount(counter);
+            return base.OnConnected();
+        }
+
+        public override System.Threading.Tasks.Task OnReconnected()
+        {
+            counter = counter + 1;
+            Clients.All.UpdateCount(counter);
+            return base.OnReconnected();
+        }
+
+        public override System.Threading.Tasks.Task OnDisconnected(bool stopCalled)
+        {
+            counter = counter - 1;
+            Clients.All.UpdateCount(counter);
+            return base.OnDisconnected(stopCalled);
+        }
+
+
+    }
+}
